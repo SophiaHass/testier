7ae160657eb9c1a9aae1146466761849d672049d
Sophia <sophiarachelhass@gmail.com>
2021-02-17 13:10:04 +0100

    Fixed mistake with a rollback. ready for next lesson

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_172945) do
+ActiveRecord::Schema.define(version: 2021_02_06_171010) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "created_at1"
-    t.datetime "updated_at1"
   end
 
 end

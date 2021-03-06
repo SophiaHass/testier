f2eac2042b88ba8fdda4ce94b388cd3bdfcaf9a3
Sophia <sophiarachelhass@gmail.com>
2021-03-06 17:45:12 +0100

    added more links, started refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
 
+before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
-        
-      @article = Article.find(params[:id])
     
     end
 
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-      @article = Article.find(params[:id])
+      
     end
 
     def create
@@ -31,7 +31,6 @@ class ArticlesController < ApplicationController
     end
 
     def update
-      @article = Article.find(params[:id])
         if @article.update(params.require(:article).permit(:title, :description))
           flash[:notice] = "Article was updated successfully"
           redirect_to(@article)
@@ -41,9 +40,14 @@ class ArticlesController < ApplicationController
       end
 
       def destroy 
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path
+      end
+
+      private #why private? Mashrur did it but I don't know why... 
+
+      def set_article
+        @article = Article.find(params[:id])
       end
 
 end
